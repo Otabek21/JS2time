0ca302a0084f8275560355617e4d96d76d633150
Otabek <25@mail.ru>
2021-12-18 23:16:09 +0500

    time1

---
+++ script.js
@@ -0,0 +1,79 @@
+
+var время = +prompt('Введите время')
+
+switch (время) {
+    case 1:
+        alert('1 ночи');
+        break;
+    case 2:
+        alert('2 часа ночи');
+        break;
+    case 3:
+        alert('3 часа ночи');
+        break;
+    case 4:
+        alert('4 часа ночи');
+        break;
+    case 5:
+        alert('5 часов утра');
+        break;
+    case 6:
+        alert('6 часов утра');
+        break;
+    case 7:
+        alert('7 часов утра. Пора вставать');
+        break;
+    case 8:
+        alert('8 часов утра. Опаздаеш на работу');
+        break;
+    case 9:
+        alert('9 часоов утра. Ели успел');
+        break;
+    case 10:
+        alert('10 часов утра');
+        break;
+    case 11:
+        alert('11 утра');
+        break;
+    case 12:
+        alert('12 полдень');
+        break;
+    case 13:
+        alert('Час дня. Наконецта обед');
+        break;
+    case 14:
+        alert('2 часа дня. Пора заканчивать обед');
+        break;
+    case 15:
+        alert('3 часа дня');
+        break;
+    case 16:
+        alert('4 часа дня');
+        break;
+    case 17:
+        alert('5 часа дня');
+        break;
+    case 18:
+        alert('6 вечера');
+        break;
+    case 19:
+        alert('7 вечера');
+        break;
+    case 20:
+        alert('8 вечера');
+        break;
+    case 21:
+        alert('9 вечера');
+        break;
+    case 22:
+        alert('10 вечера');
+        break;
+    case 23:
+        alert('11 вечера');
+        break;
+    case 24:
+        alert('Полночь');
+        break;   
+    default:
+        break;
+}
